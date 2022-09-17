3f4fdc7afb17dd4455ba2b3dc89d038d9764bdd9
Mamtha0423 <113755833+Mamtha0423@users.noreply.github.com>
2022-09-17 08:24:37 -0500

    Add files via upload
    mamtha files

--- code.ino
+++ code.ino
@@ -1,29 +1,28 @@
 int moisture_data=0; 
 void setup()
-{
-  pinMode(A0,INPUT); 
+{ 
+  pinMode(A0,INPUT);
   Serial.begin(9600); 
-  pinMode(12,OUTPUT); 
-  pinMode(2,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(4,OUTPUT);
 
 }
 
-void loop() 
+void loop()
 {
 moisture_data= analogRead(A0);
-  Serial.println(moisture_data);
+Serial.println(moisture_data);
 
-if(moisture_data<22){ 
-  digitalWrite(12,HIGH); 
-  digitalWrite(2,HIGH);
+if(moisture_data<22){
+  digitalWrite(8,HIGH); 
+  digitalWrite(4,HIGH);
 
 }
 
 else{ 
-  digitalWrite(12,LOW); 
-  digitalWrite(2,LOW);
+  digitalWrite(8, LOW); 
+  digitalWrite(4, LOW);
 
-}
+} 
   delay(5);
-
 }
